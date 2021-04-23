2a87ce7559db9ba2180ceec464b3bf35125abb7d
aly323 <62492391+aly323@users.noreply.github.com>
2021-04-22 22:01:31 -0400

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,11 +1,18 @@
 var progress = 0;
 
 // update progress bar
-function updateProgress() {
-    var instructionNum = 8;
-    var increment = 100 / instructionNum;
-    var newprogress = progress + increment;
+function updateProgress(stepNum, isCrossed) {
+    //var instructionNum = 8;
+    //var increment = 100 / instructionNum;
+    //var newprogress = progress + increment;
 
+    var increment = 10;
+    var newprogress;
+
+    if (isCrossed != true)
+        newprogress = progress + increment;
+    else
+        newprogress = progress - increment;
 
     progress = newprogress;
     // change how much the bar is filled
@@ -13,4 +20,22 @@ function updateProgress() {
 
     // change text on the bar
     document.getElementById("progress-bar").innerHTML = newprogress + "%";
+}
+
+// will cross-off and change the text color of the completed steps
+function crossoff(stepNum, clr) {
+
+    // not yet crossed off...
+    if (stepNum.style.color != clr) {
+        stepNum.style.color = clr;
+        stepNum.style.textDecoration = "line-through";
+
+        updateProgress(stepNum, false);
+
+    } else {
+        stepNum.style.color = 'black';
+        stepNum.style.textDecoration = "none";
+
+        updateProgress(stepNum, true);
+    }
 }
